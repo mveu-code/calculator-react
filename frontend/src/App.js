7645be77afb0f705cb1656620ecda288ed516f95
Egor Gavrilov <gavrilovegor519@gmail.com>
2024-04-07 18:09:55 +0300

    Create start layout of the app.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,15 @@
+import React from 'react';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import './App.css';
+import Main from './views/Main';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='' element={<Main/>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/views/Main.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import './Main.css';
+
+function Main() {
+  return (
+    <div className='Main'>
+        <Header/>
+        <div className='content'>
+            
+        </div>
+        <Footer/>
+    </div>
+  );
+}
+
+export default Main;
