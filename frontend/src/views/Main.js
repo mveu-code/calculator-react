a7ab4a71132e0ef822caebbb3877feab06da0289
zerodbee <125971837+zerodbee@users.noreply.github.com>
2024-04-11 12:31:17 +0300

    calculator link

--- frontend/src/views/Main.js
+++ frontend/src/views/Main.js
@@ -1,18 +1,32 @@
-import React from 'react';
-import Header from '../components/Header';
-import Footer from '../components/Footer';
+import { Route, Routes, Link } from 'react-router-dom';
 import './Main.css';
 
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import React from 'react';
+
+const CalcObject = {
+  id: 1,
+  name: 'Калькулятор рассрочки'
+}
+
 function Main() {
-  return (
-    <div className='Main'>
-        <Header/>
-        <div className='content'>
-            <h1>Главная страница</h1>
-        </div>
-        <Footer/>
+
+      return (
+        <>
+    <div className="Main">
+      <Header />
+        <div className="info1">
+
+              <p className="link1">{CalcObject.name}</p>
+
+            <a href="/Calc" className='btn'>Пeрейти</a>
+          </div>
     </div>
+    <Footer />
+    </>
   );
+
 }
 
 export default Main;
