d63d5c4938367efaf4c8c4df60ee54e2e10a0554
zerodbee <oborotova.vika.00@gmail.com>
2024-04-18 15:13:36 +0300

    interfaceCalc

--- frontend/src/views/InterfaceCalc.js
+++ frontend/src/views/InterfaceCalc.js
@@ -1,41 +1,61 @@
-import Header from '../components/Header';
-import { useState, useEffect } from 'react';
-import Footer from '../components/Footer';
-import { useParams } from 'react-router-dom';
+import Header from "../components/Header";
+import { useState, useEffect } from "react";
+import Footer from "../components/Footer";
+import { useParams } from "react-router-dom";
 import CalculatorItem from "../components/CalculatorItem";
 
-import React from 'react';
+import React from "react";
 
-function InterfaceCalc() {
-    const {id} = useParams();
+function InterfaceCalc({ nameCalc }) {
+  const [calc, setCalc] = useState([]);
 
-    const [calc, setCalc] = useState([])
+  useEffect(() => {
+    const api = `http://127.0.0.1:9001/calculator/get/all`;
 
-    useEffect(() => {
-        const api = (`http://127.0.0.1:9001/calculator/get/all`)
+    fetch(api)
+      .then((res) => res.json())
+      .then((result) => {
+        setCalc(result.data);
+      });
+  }, []);
 
-        fetch(api)
-            .then((result) => result.json())
-            .then((result) => {
+  let element;
+  function processElement(currentElement) {
+    element = currentElement;
+  }
+  calc.forEach(processElement);
+  console.log(element);
 
-                setCalc(result.data)
-            })
-    }, [])
+  var variables = {
+    x: 5,
+  };
 
-    return (
-        <>
-            <Header />
-            <div>
-
-                <p>{id}</p>
-                
-                <input id="x" type="number" placeholder="Введите первое число" min="0"></input>
-                <input id="y" type="number" placeholder="Введите второе число" min="0"></input>
-                <button id="result">Вычислить</button>
-            </div>
-            <Footer />
-        </>
-    );
+  //var expression = math.parse('a+b+c');
+  // console.log(expression);
+  if (element) {
+  return (
+    <>
+      <Header />
+      <div>
+        <p>{element.nameCalc}</p>
+        <input
+          id="x"
+          type="number"
+          placeholder="Введите первое число"
+          min="0"
+        ></input>
+        <input
+          id="y"
+          type="number"
+          placeholder="Введите второе число"
+          min="0"
+        ></input>
+        <button id="result">Вычислить</button>
+      </div>
+      <Footer />
+    </>
+  );
+}
 }
 
 export default InterfaceCalc;
