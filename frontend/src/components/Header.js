129683db0554f4730ab3439c44e9b1d31ee35bd2
Egor Gavrilov <gavrilovegor519@gmail.com>
2024-04-29 18:40:34 +0300

    More refactorings.

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,11 +1,37 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import './Header.css'
 
 function Header() {
+  const [isLogin, setIsLogin] = useState(false)
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if (localStorage.getItem('jwt') !== null) {
+      setIsLogin(true)
+    }
+  }, [])
+
+  const logout = () => {
+    localStorage.removeItem('jwt')
+    setIsLogin(false)
+    navigate('/')
+  }
+
+  const LogoutButton = () => {
+    if (isLogin === true) {
+      return (
+        <div className='LogoutButton'>
+          <button onClick={logout}>Выход из админки</button>
+        </div>
+      )
+    }
+  }
+
   return (
     <div className='Header'>
         <Link className="HeaderLink" to="/">Кредитные калькуляторы</Link>
+        <LogoutButton />
     </div>
   );
 }
